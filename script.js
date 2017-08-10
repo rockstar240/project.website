a492fcace5c47040c44ac65c224ee26f4ac38c22
Hack Reactor Student <student@hackreactor.com>
2017-08-10 16:35:06 -0500

    Project

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-<DonnieYen/>
+
 //What does the line $(document).ready(function(){ do? Write your answer as a comment below
 //
 $(document).ready(function(){
 // your code goes below
+$("CLICK HERE").click(function(){
+  $("p").show;
+
+});
+
 })
